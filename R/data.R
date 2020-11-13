38fa3d8fe8a1008c5a4891a4b7a677c7ec63c38e
Zhiwen Tan <leon.tan@mail.utoronto.ca>
2020-11-13 18:15:17 -0500

    add some data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Part of miRNA sequence data from miRBase
+#'
+#' A dataset contains 14 different miRNA
+#'
+#' @source \url{http://www.mirbase.org/}
+#'
+#' @format A matrix with columns:
+#' \describe{
+#'  \item{id}{name for the miRNA}
+#'  \item{sequences}{miRNA sequence}
+#' }
+#' @examples
+#' \dontrun{
+#'  ExampleSequence
+#' }
+"ExampleSequence"
+
+# [END]
